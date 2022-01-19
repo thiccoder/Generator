c8f6ba4b78bb21252cb89458c1414f24dd4a6f47
thiccoder <dom250506@gmail.com>
2022-01-19 22:17:15 +0300

    Добавьте файлы проекта.

---
+++ Generator/MainForm.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace Generator
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mainSplit = new System.Windows.Forms.SplitContainer();
+            this.splitContainer = new System.Windows.Forms.SplitContainer();
+            this.treeView1 = new System.Windows.Forms.TreeView();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.generateButton = new System.Windows.Forms.ToolStripButton();
+            ((System.ComponentModel.ISupportInitialize)(this.mainSplit)).BeginInit();
+            this.mainSplit.Panel1.SuspendLayout();
+            this.mainSplit.Panel2.SuspendLayout();
+            this.mainSplit.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).BeginInit();
+            this.splitContainer.Panel2.SuspendLayout();
+            this.splitContainer.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mainSplit
+            // 
+            this.mainSplit.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.mainSplit.Location = new System.Drawing.Point(0, 0);
+            this.mainSplit.Name = "mainSplit";
+            this.mainSplit.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // mainSplit.Panel1
+            // 
+            this.mainSplit.Panel1.Controls.Add(this.splitContainer);
+            // 
+            // mainSplit.Panel2
+            // 
+            this.mainSplit.Panel2.Controls.Add(this.toolStrip1);
+            this.mainSplit.Size = new System.Drawing.Size(775, 489);
+            this.mainSplit.SplitterDistance = 455;
+            this.mainSplit.TabIndex = 1;
+            // 
+            // splitContainer
+            // 
+            this.splitContainer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.splitContainer.Location = new System.Drawing.Point(0, 0);
+            this.splitContainer.Name = "splitContainer";
+            // 
+            // splitContainer.Panel2
+            // 
+            this.splitContainer.Panel2.Controls.Add(this.treeView1);
+            this.splitContainer.Size = new System.Drawing.Size(775, 455);
+            this.splitContainer.SplitterDistance = 500;
+            this.splitContainer.TabIndex = 0;
+            // 
+            // treeView1
+            // 
+            this.treeView1.BackColor = System.Drawing.SystemColors.Control;
+            this.treeView1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.treeView1.HideSelection = false;
+            this.treeView1.LineColor = System.Drawing.Color.Gray;
+            this.treeView1.Location = new System.Drawing.Point(0, 0);
+            this.treeView1.Name = "treeView1";
+            this.treeView1.Size = new System.Drawing.Size(271, 455);
+            this.treeView1.TabIndex = 0;
+            this.treeView1.AfterSelect += new System.Windows.Forms.TreeViewEventHandler(this.TreeView_AfterSelect);
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.BackColor = System.Drawing.SystemColors.Control;
+            this.toolStrip1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.toolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.generateButton});
+            this.toolStrip1.LayoutStyle = System.Windows.Forms.ToolStripLayoutStyle.HorizontalStackWithOverflow;
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(775, 30);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip";
+            // 
+            // generateButton
+            // 
+            this.generateButton.BackColor = System.Drawing.SystemColors.Control;
+            this.generateButton.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.generateButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.generateButton.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.generateButton.ImageScaling = System.Windows.Forms.ToolStripItemImageScaling.None;
+            this.generateButton.ImageTransparentColor = System.Drawing.SystemColors.ControlText;
+            this.generateButton.Name = "generateButton";
+            this.generateButton.Size = new System.Drawing.Size(58, 27);
+            this.generateButton.Text = "Generate";
+            this.generateButton.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.generateButton.Click += new System.EventHandler(this.Generate);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(775, 489);
+            this.Controls.Add(this.mainSplit);
+            this.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.MaximumSize = new System.Drawing.Size(791, 528);
+            this.Name = "MainForm";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            this.mainSplit.Panel1.ResumeLayout(false);
+            this.mainSplit.Panel2.ResumeLayout(false);
+            this.mainSplit.Panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.mainSplit)).EndInit();
+            this.mainSplit.ResumeLayout(false);
+            this.splitContainer.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer)).EndInit();
+            this.splitContainer.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.SplitContainer mainSplit;
+        private System.Windows.Forms.SplitContainer splitContainer;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton generateButton;
+        private System.Windows.Forms.TreeView treeView1;
+    }
+}
+
---
+++ Generator/MainForm.cs
@@ -0,0 +1,168 @@
+﻿using Word = Microsoft.Office.Interop.Word;
+using System.Xml;
+
+namespace Generator
+{
+    public partial class MainForm : Form
+    {
+        public List<Field> fields = new();
+        public List<TreeNode> treeNodes = new();
+        public List<Control> controls = new();
+        public int currentIdx;
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+        public void ParseNodes(XmlNode xmlNode)
+        {
+            if (xmlNode.Name == "field")
+            {
+                fields.Add(new Field(xmlNode));
+            }
+            else foreach (XmlNode node in xmlNode.ChildNodes)
+                {
+                    if (node.HasChildNodes)
+                        ParseNodes(node);
+                }
+        }
+        public void ReadXML()
+        {
+            XmlDocument doc = new();
+            string fileName = Environment.CurrentDirectory + "\\Template.xml";
+            doc.Load(fileName);
+            treeView1.Nodes.Clear();
+            ParseNodes(doc.DocumentElement);
+            TreeNode[] nodes = new TreeNode[fields.Count];
+            for (int i = 0; i < nodes.Length; i++)
+            {
+                nodes[i] = new TreeNode(fields[i].Name);
+                treeNodes.Add(nodes[i]);
+                Control control;
+                switch (fields[i].Type)
+                {
+                    case FieldType.Number:
+                        NumericUpDown TBN = new()
+                        {
+                            AllowDrop = true,
+                            BackColor = SystemColors.Control,
+                            Dock = DockStyle.Fill,
+                            Location = new Point(0, 0),
+                            Name = "control",
+                            Size = new Size(603, 483),
+                            TabIndex = 0,
+                            Enabled = false,
+                            Visible = false
+                        };
+                        control = TBN;
+                        break;
+                    case FieldType.Date:
+                        MonthCalendar TBD = new()
+                        {
+                            CalendarDimensions = new Size(3, 3),
+                            Dock = DockStyle.Fill,
+                            Location = new Point(0, 0),
+                            MaxSelectionCount = 1,
+                            Name = "control",
+                            TabIndex = 0,
+                            Enabled = false,
+                            Visible = false
+                        };
+                        control = TBD;
+                        break;
+                    case FieldType.Choice:
+                        ComboBox TBC = new()
+                        {
+                            Dock = DockStyle.Fill,
+                            FormattingEnabled = true,
+                            Location = new Point(0, 0),
+                            Name = "control",
+                            Size = new Size(500, 23),
+                            TabIndex = 0,
+                            Enabled = false,
+                            Visible = false
+                        };
+                        TBC.Items.AddRange(fields[i].options.ToArray());
+                        control = TBC;
+                        break;
+                    default:
+                        TextBox TBT = new()
+                        {
+                            AcceptsReturn = true,
+                            AcceptsTab = true,
+                            AllowDrop = true,
+                            BackColor = SystemColors.Control,
+                            Dock = DockStyle.Fill,
+                            Location = new Point(0, 0),
+                            Multiline = true,
+                            Name = "control",
+                            Size = new Size(603, 483),
+                            TabIndex = 0,
+                            Enabled = false,
+                            Visible = false
+                        };
+                        control = TBT;
+                        break;
+                }
+                controls.Add(control);
+                splitContainer.Panel1.Controls.Add(control);
+                //nodes[i].Nodes.Add(new TreeNode("Mask: " + fields[i].Mask));
+                //nodes[i].Nodes.Add(new TreeNode("Value: " + fields[i].Value));
+                //nodes[i].Nodes.Add(new TreeNode("Options: " + fields[i].options.ToArray().ToString()));
+                //nodes[i].Nodes.Add(new TreeNode("Type: " + fields[i].Type.ToString()));
+            }
+            treeView1.Nodes.AddRange(nodes);
+            currentIdx = 0;
+            treeView1.SelectedNode = nodes[0];
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            mainSplit.IsSplitterFixed = true;
+            ReadXML();
+        }
+
+        private void Generate(object sender, EventArgs e)
+        {
+            Word.Application app = new();
+            object fileName = Environment.CurrentDirectory + "\\Template.dotx";
+            object missing = Type.Missing;
+            app.Documents.Open(ref fileName);
+            Word.Find find = app.Selection.Find;
+            foreach (Field field in fields) 
+            {
+                find.Text = field.Mask;
+                find.Replacement.Text = field.Value;
+                object wrap = Word.WdFindWrap.wdFindContinue;
+                object replace = Word.WdReplace.wdReplaceAll;
+                find.Execute(FindText: missing,
+                    MatchCase: true,
+                    MatchWholeWord: true,
+                    MatchWildcards: false,
+                    MatchSoundsLike: missing,
+                    MatchAllWordForms: false,
+                    Forward: true,
+                    Wrap: wrap,
+                    Format: false,
+                    ReplaceWith: missing, Replace: replace);
+            }
+            app.ActiveDocument.SaveAs(FileName: Environment.CurrentDirectory + "\\Result.docx");
+            app.ActiveDocument.Close();
+            app.Quit();
+        }
+
+        private void TreeView_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+            int idx = treeNodes.IndexOf(e.Node);
+            Field cf = fields[currentIdx];
+            if (cf.Type != FieldType.Date)
+                cf.Value = controls[currentIdx].Text;
+            else cf.Value = (controls[currentIdx] as MonthCalendar).SelectionStart.ToLongDateString().Trim('.');
+            controls[currentIdx].Enabled = false;
+            controls[currentIdx].Visible = false;
+            controls[idx].Enabled = true;
+            controls[idx].Visible = true;
+            currentIdx = idx;
+
+        }
+    }
+}
